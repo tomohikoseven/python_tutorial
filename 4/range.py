67b47fc4653a4e226ad5cde08ad1e677ba07c860
tomohikoseven <tomohikoseven0905@gmail.com>
2021-07-24 17:09:58 +0900

    range.

---
+++ 4/range.py
@@ -0,0 +1,20 @@
+# for i in range(5):
+#   print(i)
+
+# a = ['Mary', 'had', 'a', 'little', 'lamb']
+# for i in range(len(a)):
+#   print(i, a[i])
+
+# for n in range(2,10):
+#   for x in range(2,n):
+#     if n % x == 0:
+#       print( n, 'equals', x, '*', n//x)
+#       break
+#   else:
+#     print(n, 'is a prime number')
+
+for num in range(2,10):
+  if num % 2 == 0:
+    print('Found an even number', num)
+    continue
+  print('Found an odd number', num)
